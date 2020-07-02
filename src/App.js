09b1fdbffa5fb93a5a3c70bdbdc4a361dfbb5cee
Lucas Barbosa Fonseca <lufonsecabarbosa@hotmail.com>
2020-07-02 17:06:37 -0300

    Desafio de React.js do Bootcamp Gostack

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post("repositories", {
+      title: `http://github.com/${Date.now()}`,
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    api.delete(`repositories/${id}`);
+
+    const repositoriesAll = repositories.filter((repo) => {
+      return repo.id !== id;
+    });
+
+    setRepositories(repositoriesAll);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
